9ca84e4360434e54427ab152d2ba9e48371d46ae
YASH GARG <125874060+Yash28102@users.noreply.github.com>
2026-06-08 13:52:51 +0530

    Update DriverFactory.java

--- Guru99BankFramework/src/test/java/base/DriverFactory.java
+++ Guru99BankFramework/src/test/java/base/DriverFactory.java
@@ -10,57 +10,90 @@ import org.openqa.selenium.edge.EdgeOptions;
 
 public class DriverFactory {
 
-private static WebDriver driver;
+    private static WebDriver driver;
 
-public static WebDriver initDriver(String browser) {
+    public static WebDriver initDriver(String browser) {
 
-    if (browser.equalsIgnoreCase("edge")) {
+        boolean isJenkins = System.getenv("JENKINS_HOME") != null;
 
-        // Path to your EdgeDriver executable
-        System.setProperty(
-                "webdriver.edge.driver",
-                "C:\\Drivers\\msedgedriver.exe"
-        );
+        if (browser.equalsIgnoreCase("edge")) {
 
-        EdgeOptions options = new EdgeOptions();
-        options.addArguments("--headless=new");
-    options.addArguments("--disable-gpu");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--remote-allow-origins=*");
-    options.addArguments("--disable-blink-features=AutomationControlled");
-    options.addArguments("--window-size=1920,1080");
-        driver = new EdgeDriver(options);
+            System.setProperty(
+                    "webdriver.edge.driver",
+                    "C:\\Drivers\\msedgedriver.exe"
+            );
 
-    } else if (browser.equalsIgnoreCase("chrome")) {
+            EdgeOptions options = new EdgeOptions();
 
-        // Path to ChromeDriver (optional if using WebDriverManager)
-        System.setProperty(
-                "webdriver.chrome.driver",
-                "C:\\Drivers\\chromedriver.exe"
-        );
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--disable-blink-features=AutomationControlled");
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-blink-features=AutomationControlled");
-        options.addArguments("--start-maximized");
+            if (isJenkins) {
+                System.out.println("Running Edge in Jenkins Headless Mode");
 
-        driver = new ChromeDriver(options);
+                options.addArguments("--headless");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
+            } else {
+                System.out.println("Running Edge in Local Mode");
 
-    } else {
-        throw new RuntimeException("Unsupported Browser: " + browser);
+                options.addArguments("--start-maximized");
+            }
+
+            driver = new EdgeDriver(options);
+
+        }
+
+        else if (browser.equalsIgnoreCase("chrome")) {
+
+            System.setProperty(
+                    "webdriver.chrome.driver",
+                    "C:\\Drivers\\chromedriver.exe"
+            );
+
+            ChromeOptions options = new ChromeOptions();
+
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--disable-blink-features=AutomationControlled");
+
+            if (isJenkins) {
+                System.out.println("Running Chrome in Jenkins Headless Mode");
+
+                options.addArguments("--headless");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
+            } else {
+                System.out.println("Running Chrome in Local Mode");
+
+                options.addArguments("--start-maximized");
+            }
+
+            driver = new ChromeDriver(options);
+
+        }
+
+        else {
+            throw new RuntimeException("Unsupported Browser: " + browser);
+        }
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+
+        return driver;
     }
 
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-
-    return driver;
-}
-
-public static void quitDriver() {
-    if (driver != null) {
-        driver.quit();
-        driver = null;
+    public static WebDriver getDriver() {
+        return driver;
     }
-}
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
